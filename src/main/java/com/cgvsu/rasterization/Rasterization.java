678469224cd6f62a27fa7ebcb152acd6257000eb
Kykyyta <izvekov-denis@bk.ru>
2025-10-06 01:35:31 +0300

    commit comment

--- src/main/java/com/cgvsu/rasterization/Rasterization.java
+++ src/main/java/com/cgvsu/rasterization/Rasterization.java
@@ -4,15 +4,16 @@ import com.cgvsu.rasterization.render.CanvasContext;
 import com.cgvsu.rasterization.render.Renderer;
 import javafx.scene.canvas.GraphicsContext;
 
-/**
- * Фасадный класс для упрощенного доступа к функциональности растеризации
- */
+// класс для упрощенного доступа к функциональности растеризации
+
 public class Rasterization {
 
     public static void drawBezierCurve(GraphicsContext gc, int width, int height,
                                        com.cgvsu.rasterization.model.BezierCurve curve) {
-        CanvasContext context = new CanvasContext(gc, width, height);
-        Renderer renderer = new Renderer(context);
-        renderer.renderScene(curve);
+
+        CanvasContext context = new CanvasContext(gc, width, height); // без этого пришлось бы работать напрямую с GraphicsContext
+
+        Renderer renderer = new Renderer(context); // создание рендера
+        renderer.renderScene(curve); // отрисовка всей сцены
     }
 }
