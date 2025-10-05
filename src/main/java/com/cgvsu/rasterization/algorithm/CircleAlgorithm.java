5e352862c7e61c7e964d7408e33f56a0922758eb
Kykyyta <izvekov-denis@bk.ru>
2025-10-06 01:34:54 +0300

    commit comment

--- src/main/java/com/cgvsu/rasterization/algorithm/CircleAlgorithm.java
+++ src/main/java/com/cgvsu/rasterization/algorithm/CircleAlgorithm.java
@@ -3,10 +3,10 @@ package com.cgvsu.rasterization.algorithm;
 import com.cgvsu.rasterization.render.CanvasContext;
 import javafx.scene.paint.Color;
 
-/**
- * Алгоритм рисования заполненного круга
- */
+// Алгоритм рисования заполненного круга
+
 public class CircleAlgorithm implements DrawingAlgorithm {
+
     private final int centerX, centerY, radius;
 
     public CircleAlgorithm(int centerX, int centerY, int radius) {
@@ -17,9 +17,11 @@ public class CircleAlgorithm implements DrawingAlgorithm {
 
     @Override
     public void draw(CanvasContext context, Color color) {
+
         int r2 = radius * radius;
-        for (int y = -radius; y <= radius; y++) {
-            for (int x = -radius; x <= radius; x++) {
+
+        for (int y = -radius; y <= radius; y++) { // вертикально
+            for (int x = -radius; x <= radius; x++) { // горизонтально
                 if (x * x + y * y <= r2) {
                     context.setPixel(centerX + x, centerY + y, color);
                 }
