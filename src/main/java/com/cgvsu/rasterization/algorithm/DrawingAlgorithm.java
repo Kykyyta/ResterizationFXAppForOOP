7ca5bf2840e5b72f3b56547f2866a82232ddb605
Kykyyta <izvekov-denis@bk.ru>
2025-10-06 01:35:00 +0300

    commit comment

--- src/main/java/com/cgvsu/rasterization/algorithm/DrawingAlgorithm.java
+++ src/main/java/com/cgvsu/rasterization/algorithm/DrawingAlgorithm.java
@@ -3,9 +3,10 @@ package com.cgvsu.rasterization.algorithm;
 import com.cgvsu.rasterization.render.CanvasContext;
 import javafx.scene.paint.Color;
 
-/**
- * Интерфейс для всех алгоритмов рисования
- */
+// Интерфейс для всех алгоритмов рисования
+
 public interface DrawingAlgorithm {
+
     void draw(CanvasContext context, Color color);
+
 }
