b59ef66635de5b881efac28c28947fb747041919
Kykyyta <izvekov-denis@bk.ru>
2025-11-16 20:18:45 +0300

    delay comm

--- src/test/java/com/cgvsu/rasterization/algorithm/TestCanvasContext.java
+++ src/test/java/com/cgvsu/rasterization/algorithm/TestCanvasContext.java
@@ -7,32 +7,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class TestCanvasContext extends CanvasContext {
+
     private final Set<String> setPixels = new HashSet<>();
     private int pixelSetCount = 0;
 
     public TestCanvasContext() {
-        // Создаем минимальный GraphicsContext чтобы избежать NPE
         super(createMinimalGraphicsContext(), 100, 100);
     }
 
-    public TestCanvasContext(int width, int height) {
-        super(createMinimalGraphicsContext(), width, height);
-    }
-
     private static GraphicsContext createMinimalGraphicsContext() {
-        // Создаем минимальный Canvas чтобы избежать NPE в GraphicsContext
         javafx.scene.canvas.Canvas canvas = new javafx.scene.canvas.Canvas(1, 1);
         return canvas.getGraphicsContext2D();
     }
 
-    // Переопределяем setPixel чтобы записывать пиксели
-    public void setPixel(int x, int y, Color color) {
-        // Записываем все пиксели, игнорируя границы для тестирования
+    public void setPixel(int x, int y, Color color) { // вместо реального пикселя просто запоминаем координаты
         setPixels.add(x + "," + y);
         pixelSetCount++;
     }
 
-    public boolean wasPixelSet(int x, int y) {
+    public boolean wasPixelSet(int x, int y) { // был ли нарисован пиксель
         return setPixels.contains(x + "," + y);
     }
 
@@ -40,8 +33,4 @@ public class TestCanvasContext extends CanvasContext {
         return pixelSetCount;
     }
 
-    public void clearRecordedPixels() {
-        setPixels.clear();
-        pixelSetCount = 0;
-    }
 }
